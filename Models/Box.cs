e072c4ca397912e43b715b07d874a794cfc93ad0
Alexey Tyapkin <Marveleks@gmail.com>
2024-10-31 19:16:37 +0300

    Initial commit

---
+++ Models/Box.cs
@@ -0,0 +1,52 @@
+﻿using Pallets.Interfaces;
+
+namespace Pallets.Models
+{
+    public class Box : IVolumeable
+    {
+        public Guid Id { get; set; }
+        public decimal Width { get; set; }
+        public decimal Height { get; set; }
+        public decimal Depth { get; set; }
+        public decimal Weight { get; set; }
+        public DateOnly ShelfLife { get { return ShelfLife; } private set { ShelfLife = value; } }
+
+        public static Box CreateBoxByProductionDate(int width, int height, int depth, int weight, DateOnly productionDate)
+        {
+            return new Box
+            {
+                Width = width,
+                Height = height,
+                Depth = depth,
+                Weight = weight,
+                ShelfLife = productionDate.AddDays(100)
+            };
+        }
+
+        public static Box CreateBoxByShelfLife(int width, int height, int depth, int weight, DateOnly shelfLife)
+        {
+            return new Box
+            {
+                Width = width,
+                Height = height,
+                Depth = depth,
+                Weight = weight,
+                ShelfLife = shelfLife
+            };
+        }
+
+        public long GetVolume()
+        {
+            try
+            {
+                return (long)(Width * Height * Depth);
+            }
+            catch
+            {
+                Console.WriteLine("Введены некорректные размеры");
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Pallets.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите, сколько палетов сгенерировать: ");
+        while(!int.TryParse(Console.ReadLine(), out var count))
+        {
+            Console.WriteLine("Некорректное значение.");
+        }
+        Console.WriteLine("Генерация палетов...");
+        Console.WriteLine("");
+    }
+}
