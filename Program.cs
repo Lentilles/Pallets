291249d7cef7e8ed048721b898c977d1c421304f
Alexey <Marveleks@gmail.com>
2024-11-03 19:36:17 +0300

    Старт программы

--- Program.cs
+++ Program.cs
@@ -1,15 +1,54 @@
-﻿using Pallets.Models;
+﻿using Pallets.Generator;
+using Pallets.Models;
+using Pallets.Views;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Введите, сколько палетов сгенерировать: ");
-        while(!int.TryParse(Console.ReadLine(), out var count))
+        List<Pallet> pallets = new List<Pallet>();
+
+
+        var menuVariant = BaseMenu.ShowMenu();
+
+        switch (menuVariant)
         {
-            Console.WriteLine("Некорректное значение.");
+            case 1:
+                {
+                    var palletCount = GenerationMenu.AskGenerationCount();
+
+                    PalletGenerator palletGenerator = new PalletGenerator(Size.DefaultMinSize, Size.DefaultMaxSize);
+                    pallets.AddRange(palletGenerator.Generate(palletCount));
+
+                    var maxBoxCount = GenerationMenu.AskGenerationCount();
+
+                    BoxGenerator boxGenerator = new BoxGenerator(Size.DefaultMinSize, Size.DefaultMaxSize, 366);
+                    Random random = new Random();
+
+                    foreach(var pallet in pallets)
+                    {
+                        if (pallet.TryAddBoxRange(boxGenerator.Generate(random.Next(maxBoxCount))))
+                        {
+                            
+                        }
+                    }
+
+
+                    break;
+                }
+            case 2:
+                {
+                    
+
+                    break;
+                }
+            case 3:
+                {
+                    
+
+                    break;
+                }
+
         }
-        Console.WriteLine("Генерация палетов...");
-        Console.WriteLine("");
     }
 }
