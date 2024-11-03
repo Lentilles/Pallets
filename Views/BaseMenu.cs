a1b743ebd5c3908b2641fcd3134d29c77173049d
Alexey <Marveleks@gmail.com>
2024-11-03 19:34:49 +0300

    Добавил консольные диалоги

---
+++ Views/BaseMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace Pallets.Views
+{
+    public static class BaseMenu
+    {
+        private const string ChoseText = "Выберите пункт меню:";
+        private const string GenerateDataText = "1. Сгенерировать паллеты и коробки";
+        private const string ShowGroupedText = "2. Сгруппировать паллеты по сроку годности, " +
+            "отсортировать по возрастанию срока годности, " +
+            "в каждой группе отсортировать паллеты по весу";
+        private const string ShowPalletsText = "3. 3 паллеты, которые содержат коробки с наибольшим сроком годности, " +
+            "отсортированные по возрастанию объема.";
+
+        private const string ParseErrorText = "Выберите пункт меню (1-3)";
+
+        public static int ShowMenu()
+        {
+            StringBuilder choseOptions = new StringBuilder();
+
+            choseOptions.AppendLine(ChoseText)
+                .AppendLine(GenerateDataText)
+                .AppendLine(ShowGroupedText)
+                .AppendLine(ShowPalletsText);
+
+            Console.WriteLine(choseOptions);
+
+            var input = Console.ReadLine();
+            var chose = 0;
+
+            while (!int.TryParse(input, out chose))
+            {
+                Console.WriteLine(ParseErrorText);
+            }
+
+            return chose;
+        }
+    }
+}
---
+++ Views/GenerationMenu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pallets.Views
+{
+    public static class GenerationMenu
+    {
+        private const string AskGenerationCountText = "Введите количество для генерации: ";
+        private const string AskGenerationCountErrorText = "Введите целое число.";
+
+        private const string GenerationSuccess = "Генерация прошла успешно.";
+        private const string GenerationFailure = "Ошибка генерации";
+        public static int AskGenerationCount()
+        {
+            int count = 0;
+            Console.WriteLine(AskGenerationCountText);
+
+            while (!int.TryParse(Console.ReadLine(), out count))
+            {
+                Console.WriteLine(AskGenerationCountErrorText);
+            }
+
+            return count;
+        }
+    }
+}
