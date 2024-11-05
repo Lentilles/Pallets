f715659fe17a3f1d1955ae653a285d608c18d6da
Alexey Tyapkin <Marveleks@gmail.com>
2024-11-06 00:05:15 +0300

    Имплементировал IGrouping

---
+++ Services/Grouping.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+namespace Pallets.Services
+{
+    public class Grouping<TKey, TElement> : IGrouping<TKey, TElement>
+    {
+        private readonly TKey key;
+        private readonly IEnumerable<TElement> values;
+
+        public Grouping(TKey key, IEnumerable<TElement> values)
+        {
+            if (values == null)
+                throw new ArgumentNullException("values");
+            this.key = key;
+            this.values = values;
+        }
+
+        public TKey Key
+        {
+            get { return key; }
+        }
+
+        public IEnumerator<TElement> GetEnumerator()
+        {
+            return values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
